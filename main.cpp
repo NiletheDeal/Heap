ed0c37b38dfd8ee8b8a9ffcd6b2d12db2cda6221
U-BSD\420607 <420607@SHS-GYBF6X3.beaverton.k12.or.us>
2024-02-23 12:23:28 -0800

    Debugging, problem with either print or adding to maxheap array

--- main.cpp
+++ main.cpp
@@ -7,15 +7,23 @@ Resources
 
 using namespace std;
 
+void print(int index, int count, int end, int heap[]);
+void HEAP(int values[], int heap[], int size);
+int max(int arr[]);
+int REMOVEROOT(int heap[], int size);
+void childCheck(int heap[], int parent);
+
 int main() {
   bool running = true;
   char command[10];
   char input [10000];
   char fileName[30];
   int values[101];
+  int heap[101];
   int totalNum = 0;
   for (int i = 0; i < 100; i++) {
     values[i] = 0;
+    heap[i] = 0;
   }
   int count = 0;
   cout << "Looping" << endl;
@@ -24,7 +32,7 @@ int main() {
       values[i] = 0;
     }
     count = 0;
-    cout << "FILE or MANUAL" << endl;
+    cout << "FILE, MANUAL, REMOVEROOT, REMOVEALL, PRINT, QUIT" << endl;
     cin.get(command, 10);
     cin.clear();
     cin.ignore(10000, '\n');
@@ -34,9 +42,10 @@ int main() {
       while(sizecheck == true) {
 	cout << "How many numbers are you gonna input?" << endl;
 	cin >> inputNum;
-	cin.get();
+	cin.clear();
+	cin.ignore(1000, '\n');
 	cout << inputNum << endl;
-	if (inputNum > 0 && inputNum < 101) {
+	if (inputNum < 0 && inputNum > 101) {
 	  cout << "Number of inputs has to be between 1 and 100" << endl;
 	  sizecheck = false;
 	}
@@ -47,18 +56,52 @@ int main() {
       cout << "Enter each number followed by a space: " << endl;
       for(int i = totalNum; i < (totalNum+inputNum); i++) {
 	cin >> values[i];
-	//cout << values[i] << endl;
+	cout << values[i] << endl;
       }
+      cin.clear();
+      cin.ignore(10000, '\n');
       totalNum += inputNum;
-      int heap[101];
-      for(int i = 0; i < 101; i++) {
-	heap[i] = 0;
-      }
       HEAP(values, heap, totalNum);
-      
+    }
+    else if(strcmp(command, "REMOVEROOT") == 0) {
+      cout << "Removing Root" << endl;
+      totalNum = REMOVEROOT(heap, totalNum);
+    }
+    else if(strcmp(command, "REMOVEALL") == 0) {
+      cout << "Removing All" << endl;
+      int current = 1;
+      while(current < totalNum) {
+	totalNum = REMOVEROOT(heap, totalNum);
+      }
+    }
+    else if(strcmp(command, "PRINT") == 0) {
+      int index = 1;
+      int count = 0;
+      print(index, count, totalNum, heap);
+    }
+    else if(strcmp(command, "QUIT") == 0) {
+      running = false;
+    }
+    else {
+      cout <<"That is not a command!" << endl;
     }
   }
 }
+void print(int index, int count, int end, int heap[]) {
+  if(index*2 + 1 < end) {
+    print(index*2 +1, count+1, end, heap);
+  }
+  for(int i = 0; i < count; i++) {
+    cout << '\t';
+  }
+  cout << heap[index] << endl;
+  //left side
+  if(index*2 < end) {
+    print(index*2, count+1, end, heap);
+  }
+}
+  
+  
 /*void PARSE(char* in, int* modify, int& count) {
   int x = 0;
   for(int i = 0; i <strlen(in); i++) {
@@ -72,25 +115,52 @@ int main() {
 
 }
 */
-void HEAP(int* values, int* heap, int size) {//Take the biggest put it in slot 1 then you tak enext biggest put in left and then right and when both are full
-  int current = 0;
-  heap[0] = values[max(values)];
+void HEAP(int values[], int heap[], int size) {//Take the biggest put it in slot 1 then you tak enext biggest put in left and then right and when both are full
+  int current = 1;
+  heap[current] = values[max(values)];
   values[max(values)] = 0;
   while(current <= size) {
-    if(heap[2*current + 1] == 0) {//if the node has no children
-      heap[2*current + 1] = values[max(values)];//left child
+    if(heap[2*current] == 0) {//if the node has no children
+      heap[2*current] = values[max(values)];//left child
       values[max(values)] = 0;//setting to zero lets max function return the next biggest number
-      heap[2*current + 2] = values[max(values)];//right child
+      heap[2*current + 1] = values[max(values)];//right child
       values[max(values)] = 0;
     }
-    
+    current++;
   }
 }
-int max(int* arr) {
+
+int max(int arr[]) {
   int maxIndex = 0;
   for(int i = 0; i < 101; i++) {
     if(arr[maxIndex] < arr[i]) {
       maxIndex = i;
     }
   }
-  return maxIndex
+  return maxIndex;
+}
+int REMOVEROOT(int heap[], int size) {
+  cout << heap[1] << " ";
+  heap[1] = heap[size];
+  size--;
+  childCheck(heap, 1);
+  return size;
+}
+void childCheck(int heap[], int parent) {
+  int temp = 0;
+  if(heap[parent] <heap[parent*2] || heap[parent] < heap[(parent*2) + 1]) { // one of the children is bigger
+      if(heap[parent*2] >= heap[parent*2 + 1]) {//left child is bigger or equal
+	temp = heap[parent*2];
+	heap[parent*2] = heap[parent];
+	heap[parent] = heap[temp];
+	childCheck(heap, parent*2);
+      }
+      else if(heap[parent*2] < heap[parent*2 + 1]) {//right child bigger
+	temp = heap[parent*2 +1];
+	heap[parent*2 +1] = heap[parent];
+	heap[parent] = temp;
+	childCheck(heap, parent*2+1);
+      }
+  }
+}
+
